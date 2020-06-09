bcf6b652c3dd1d3de8aae586f3b21e9d328ac5ce
Saikiran Adepu <kiran.narj@gmail.com>
2020-06-09 18:03:53 -0400

    updated Scala Sample1

--- src/scalasample1.scala
+++ src/scalasample1.scala
@@ -1,5 +1,10 @@
 
 
 object scalasample1 {
-  
+  def main(args : Array[String]) : Unit = {
+     println(Listrev(l1))
+}
+def Listrev[A](s: List[A], d: List[A] = Nil): List[A] = {
+if (s == Nil) d else Listrev(s.tail, s.head :: d)
+}
 }
